4e62b250b1d8944d3f8d59626bfd1a086a6a129a
poitras <nicopoitras@gmail.com>
2018-02-02 18:54:27 -0500

    added redux worflow for booklist and bookdetail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Javascript: The good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'Lord of the ring' },
-    { title: 'The Dark Tower' }
+    { title: 'Javascript: The good Parts', detail: '111 1 1 1 1 111 ', pages: 101 },
+    { title: 'Harry Potter', detail: '222 2 2 22 2 2 2  2 2 2 2 2 2 ', pages: 134 },
+    { title: 'Lord of the ring', detail: '333 3 3 3 33  33 3 3 3 3 ' , pages: 156},
+    { title: 'The Dark Tower', detail: '44 4 4 4 4 4 444 444 4 4 4 ', pages: 178}
   ]
 }
