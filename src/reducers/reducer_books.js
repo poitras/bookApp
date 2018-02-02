c4caefda2598b401990d509cfae6f556b3f9d433
poitras <nicopoitras@gmail.com>
2018-02-01 22:17:59 -0500

    Added redux book_reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'Lord of the ring' },
+    { title: 'The Dark Tower' }
+  ]
+}
